59812f522bf1bea511663cf28ab9f8c41370fcab
Selim Yıldız <selimyildiz91@gmail.com>
2020-09-26 22:46:47 +0200

    Fixed PDF page and blurry problem

--- src/components/PDFExport/index.js
+++ src/components/PDFExport/index.js
@@ -3,27 +3,30 @@ import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
 class PDFExport extends React.Component {
-    print = async ()=>{
+    print = async () => {
         const input = document.getElementById('root');
-        // TODO: Check size and paging
+
         var pdf = new jsPDF();
         html2canvas(input, {
-            scale:0.44
-          }).then((canvas) => {
-          const imgData = canvas.toDataURL('image/png');
-          
-          pdf = new jsPDF('p', 'mm', [297, 210]);
-          
-          pdf.addImage(imgData, 'PNG', 0, 0);
-          pdf.save(`CV.pdf`);
+            scale: 2
+        }).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+
+            pdf = new jsPDF('p', 'mm', [900, 390]);
+
+            var width = pdf.internal.pageSize.getWidth();
+            var height = pdf.internal.pageSize.getHeight();
+
+            pdf.addImage(imgData, 'PNG', 0, 0, width, height);
+            pdf.save(`CV.pdf`);
         });
-      ;
+        ;
     }
 
     render() {
         return (
             <>
-            <button class="btn btn-navy btn-icon pull-right" data-html2canvas-ignore="true" onClick={()=>this.print()}><i class="jam jam-printer"></i> Export</button>
+                <button class="btn btn-navy btn-icon pull-right" data-html2canvas-ignore="true" onClick={() => this.print()}><i class="jam jam-printer"></i> Export</button>
             </>
         );
     }
